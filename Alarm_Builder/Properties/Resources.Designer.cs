11b8bb6666b323451f982279c2be7632c6e1da30
Willisfun <a0979569973@gmail.com>
2026-07-24 11:12:59 +0800

    完美的版本

--- Alarm_Builder/Properties/Resources.Designer.cs
+++ Alarm_Builder/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Alarm_to_msgxml.Properties {
+namespace Alarm_Builder.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Alarm_to_msgxml.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Alarm_to_msgxml.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Alarm_Builder.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Alarm_Builder/Properties/Settings.Designer.cs
+++ Alarm_Builder/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Alarm_to_msgxml.Properties {
+namespace Alarm_Builder.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
